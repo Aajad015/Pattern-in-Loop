beea69e50160e3c899cebca495147ddd99d46e07
Aajad015 <52133710+Aajad015@users.noreply.github.com>
2022-03-31 18:09:17 +0530

    Add files via upload

---
+++ PATTERN.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+clrscr();
+printf("Enter the Number :");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+//for(j=1;j<=i;j++)
+for(j=n;j>=i;j--)
+{
+printf("*");
+}
+printf("\n");
+}
+getch();
+}
+
